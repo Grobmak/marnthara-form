7de14e4e79c3eb78685ab56e7f11a66eba2af8b5
Grobmak <grobmak@gmail.com>
2025-09-09 12:20:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -54,7 +54,8 @@ self.addEventListener('fetch', (event) => {
                         return response;
                     }
                 );
-            })
+            }
+        )
     );
 });
 
